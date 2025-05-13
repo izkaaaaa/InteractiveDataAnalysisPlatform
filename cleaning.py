89eb7d30169f76de95cffe85c2723ac7cacb7646
HUTUTU429 <1785568564@qq.com>
2025-05-13 10:19:51 +0800

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -26,9 +26,9 @@ def clean_data1(data_top250: pd.DataFrame) -> pd.DataFrame:
     # 删除缺失值
     df.dropna(inplace=True)
     #异常值处理
-    df = df[(df['rating'] >= 0) & (df['rating'] <= 10)]
-    df = df[df['num_ratings'] > 0]
-    df = df[(df['year'] >= 1900) & (df['year'] <= 2025)]
+    df = df[(df['评分'] >= 0) & (df['评分'] <= 10)]
+    df = df[df['评分人数'] > 0]
+    df = df[(df['年份'] >= 1900) & (df['年份'] <= 2025)]
     # 重置索引
     df.reset_index(drop=True, inplace=True)
 
